545e5c8f06cac652c9ff03209e2a1ae39f926013
SnehaVerma16 <sverma16sep@gmail.com>
2026-07-22 10:12:43 +0530

    Improved Render Deployment

--- app.py
+++ app.py
@@ -225,7 +225,6 @@ import os
 
 if __name__ == "__main__":
     demo.launch(
-        server_name="127.0.0.1",
-        server_port=7860,
-        inbrowser=True
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
     )
